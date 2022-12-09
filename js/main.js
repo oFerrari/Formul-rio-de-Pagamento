4dfa8b595eb850a9e9e2b34b18da21f0c53ec7d9
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2022-12-09 17:04:54 -0300

    📝 Salvando Estado

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-import '../css/cartao-credito.css';
 import '@picocss/pico'
+import '../css/cartao-credito.css';
 
+const botao = document.querySelector('.credit-card')
 
+botao.addEventListener('click', () => {
+    alert("voce esta clicando no cartão")
+})
