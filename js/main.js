986320482249576e15dc9e2421dc2861071d35af
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2022-12-12 10:59:14 -0300

    ⚡️ Input nome

--- js/main.js
+++ js/main.js
@@ -7,15 +7,26 @@ const inputNumero = document.querySelector('#numeroCartao')
 const mostrarNumero = document.querySelector('.card-number')
 
 const inputNome = document.querySelector('#nome')
+const mostrarNome = document.querySelector('.card-holder')
+
 const inputData = document.querySelector('#data')
 const inputCvv = document.querySelector('#ccv')
 
-
+/* inputNumero.addEventListener('input',() =>{
+    mostrarNumero.innerHTML = `${inputNumero.value}`
+}) */
 
 inputNumero.addEventListener('input',() =>{
     mostrarNumero.textContent = inputNumero.value
     if(mostrarNumero.textContent.length == 4 || mostrarNumero.textContent.length == 9 || mostrarNumero.textContent.length == 14){
         inputNumero.value +=   ` `
+    }
+})
+
+inputNome.addEventListener('input',() =>{
+    mostrarNome.textContent = inputNome.value
+    if(mostrarNome.textContent.length == 10){
+        inputNome.value += `\n`
     }
 })
 
